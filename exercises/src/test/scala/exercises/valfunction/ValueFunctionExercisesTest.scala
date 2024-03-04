7b4ce311340cb0bb6d5d9d27c1f3904f95dd7185
Yiannis Sfinias <sfinias@hotmail.com>
2024-03-04 22:20:28 +0100

    Value Function Exercises

--- exercises/src/test/scala/exercises/valfunction/ValueFunctionExercisesTest.scala
+++ exercises/src/test/scala/exercises/valfunction/ValueFunctionExercisesTest.scala
@@ -10,21 +10,58 @@ class ValueFunctionExercisesTest extends AnyFunSuite with ScalaCheckDrivenProper
   // Exercise 1: String API with higher-order functions
   /////////////////////////////////////////////////////
 
-  // replace `ignore` by `test` to enable the test
-  ignore("selectDigits examples") {
+  // replace `test` by `test` to enable the test
+  test("selectDigits examples") {
     assert(selectDigits("hello4world-80") == "480")
     assert(selectDigits("welcome") == "")
   }
 
-  // replace `ignore` by `test` to enable the test
-  ignore("selectDigits length is smaller") {
+  // replace `test` by `test` to enable the test
+  test("selectDigits length is smaller") {
     forAll { (text: String) =>
       assert(selectDigits(text).length <= text.length)
+    }
+  }
+
+  test("selectDigits is aonly digits") {
+    forAll { (text: String) =>
+      selectDigits(text).foreach(c => assert(c.isDigit))
+    }
+  }
+
+  test("secret examples") {
+    assert(secret("Welcome123") == "**********")
+  }
+
+  test("secret PBT") {
+    forAll { text: String =>
+      val once = secret(text)
+      val twice = secret(once)
+      assert(once == twice)
+    }
+  }
+
+  test("isValidUsername") {
+    forAll{ username: String =>
+      assert(isValidUsername(username.toLowerCase) == isValidUsername(username.toUpperCase))
     }
   }
 
   ///////////////////////
   // Exercise 2: Point
   ///////////////////////
+
+  test("Point isPositive") {
+    forAll{ (x: Int, y: Int, z: Int) =>
+      assert(Point(x.max(0), y.max(0), z.max(0)).isPositive)
+    }
+  }
+
+  test("Point forAll") {
+    forAll{ (x: Int, y: Int, z: Int, predicate: Int => Boolean) =>
+      assert(Point(x, y, z).forAll(predicate) == List(x, y, z).forall(predicate))
+    }
+  }
+
 
 }
